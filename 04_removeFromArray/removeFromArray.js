3cfbbb9839d15706edc9699a02ac6bec22d25d4b
Murilo Pinto Alves Apparecido <murilo.apparecido@gmail.com>
2025-01-24 11:41:09 -0500

    Exercise 4 Complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...element) {
+    const newArray = []; 
+
+    for (let i = 0; i < array.length; i++){
+        if (!element.includes(array[i])){
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray; 
 };
 
 // Do not edit below this line
